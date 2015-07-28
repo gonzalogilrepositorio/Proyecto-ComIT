8bc9ab78b1f9811e895fea599e4f8a984eaae676
gonzalogilrepositorio <gonzalo_971@hotmail.com>
2015-07-28 19:26:26 -0300

    add

---
+++ Proyecto/Proyecto/Models/Property.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Property
+    {
+        //agrege un comentario
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Direccion { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public int Ambientes { get; set; }
+        public int Baños { get; set; }
+        public bool Cochera { get; set; }
+        public bool GasNatural { get; set; }
+        public bool Amoblado { get; set; }
+        public bool AireAcond { get; set; }
+        public bool Niños { get; set; }
+        public bool Mascotas { get; set; }
+        public double MtsCuadrados { get; set; }
+        public double Precio { get; set; }
+        public bool Estado { get; set; } //indica si esta disponible o no la propiedad
+        public int TiempoRestante { get; set; } //indica el tiempo restante para que caduque la publicacion
+        public DateTime FechaPublicacion { get; set; }
+
+        public virtual Locality Locality { get; set; }
+        public int LocalityId { get; set; } //indica la ciudad o pueblo donde se encuentra
+        public virtual PropertyType PropertyType { get; set; }
+        public int PropertyTypeId { get; set; } //indica si es un local, dpto, casa...
+        public virtual Category Category { get; set; }
+        public int CategoryId { get; set; } //indica si es venta, alquiler temporario o alquiler mensual
+        public virtual User User { get; set; }
+        public int UserId { get; set; } //indica el usuario que publico la propiedad
+    }
+}
