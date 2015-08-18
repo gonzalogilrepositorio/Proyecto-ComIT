72f064813934217ddd0e6655ad1da754db49b53c
Emmanuel Benzo <benzoe_89@hotmail.com>
2015-08-18 20:40:28 -0300

    userHelper

---
+++ Proyecto/Proyecto/Helpers/UserHelper.cs
@@ -0,0 +1,22 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Helpers
+{
+    public static class UserHelper
+    {
+        public static User GetUser(this System.Security.Principal.IIdentity identity){
+
+            if (identity.IsAuthenticated) {
+                using (var db = new ApplicationDbContext())
+                {
+                    return db.Users.FirstOrDefault(x => x.UserName == identity.Name);
+                }
+            }
+            return null;
+        }
+    }
+}
